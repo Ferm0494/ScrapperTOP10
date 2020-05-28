66929a0ab1eebfde5a982b7ef421807ee35b79b1
Fernando Rivas <ferm0494>
2020-05-28 09:00:16 -0600

    Linter errors

--- lib/game.rb
+++ lib/game.rb
@@ -1,42 +1,44 @@
-class Game 
-    attr_reader :title, :description, :price, :rating
-    def initialize(link)
-        @title = get_title(link.css('h2 a').text).tr("\n","").strip
-        @description = get_description(link.css('div.field.field-name-field-best-list-description').text).tr("\n","").strip
-        @price = get_price(link.css('div.price-field').text).tr("\n","").strip
-        @rating = get_rating(link.css('div.integer-stars-5').to_s,link.css('div.integer-stars-4').to_s)
-    end
+class Game
+  def initialize(link)
+    @title = title(link.css('h2 a').text).tr("\n", '').strip
+    @description = description(link.css('div.field.field-name-field-best-list-description').text).tr("\n", '').strip
+    @price = price(link.css('div.price-field').text).tr("\n", '').strip
+    @rating = rating(link.css('div.integer-stars-5').to_s, link.css('div.integer-stars-4').to_s)
+    @img = img(link.css('source').attr('data-srcset').to_s)
+  end
 
-    def get_title(title) 
-        return false if title.empty?
-        title
-    end
+  def title(title)
+    return false if title.empty?
 
-    def get_description(description)
-        
-        return false if description.empty?
-        description
-    end
+    title
+  end
 
-    def get_price(price)
-        return false if price.empty?
-        price
-    end
+  def description(description)
+    return false if description.empty?
 
-    def get_img()
-        full_elem = @link.css('source').attr('data-srcset').to_s
-        return false if full_elem.empty?
-        elems = full_elem.split(' ')
-        elems[0]
-    end
+    description
+  end
 
-    def get_rating(star5,star4)
-        if !star5.empty?
-           return 5
-        elsif !star4.empty?
-            return 4
-        else
-            return false
-        end
+  def price(price)
+    return false if price.empty?
+
+    price
+  end
+
+  def img(full_elem)
+    return false if full_elem.empty?
+
+    elems = full_elem.split(' ')
+    elems[0]
+  end
+
+  def rating(star5, star4)
+    if !star5.empty?
+      5
+    elsif !star4.empty?
+      4
+    else
+      false
     end
+  end
 end
--- lib/games.rb
+++ lib/games.rb
@@ -1,27 +1,25 @@
 require 'yaml'
 class Games
-    attr_reader :games
-    def initialize()
-        @games = Array.new
-        @count= 0
-    end
+  attr_reader :games
+  def initialize()
+    @games = []
+    @count = 0
+  end
 
-    def get_data()
-        objects = String.new
-        @games.each_with_index{
-            |data,index|
-            objects += data.to_yaml.gsub("--- !ruby/object:Game","---Game: #{index+1}----")
-        }
-        objects
+  def data()
+    objects = ''
+    @games.each_with_index do |data, index|
+      objects += data.to_yaml.gsub('--- !ruby/object:Game', "---Game: #{index + 1}----")
     end
+    objects
+  end
 
-    def push(object)
-        if object.is_a?(Object)
-                @games[@count] = object
-                @count += 1
-        else
-            return false
-        end
+  def push(object)
+    if object.is_a?(Object)
+      @games[@count] = object
+      @count += 1
+    else
+      false
     end
+  end
 end
-
